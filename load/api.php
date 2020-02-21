2703dc7f1686b6cf38e67c53115097730ec3f642
Francisco Antonio <48869903+Hornegam@users.noreply.github.com>
2020-02-21 17:53:37 -0300

    Add files via upload

---
+++ load/api.php
@@ -0,0 +1,30 @@
+
+<?php 
+    include '../model/conecta.inc';
+
+    
+
+
+    function getIp($id){
+        global $conexao;
+        $sql = "select * from job where idIp = $id";
+        return $tudo = $conexao->query($sql);
+    }
+
+    function tratar(){
+        $id =  (object) json_decode(file_get_contents('php://input'), true);
+        $ip = getIp($id->id);
+        $sla = [];
+        while($row = $ip->fetch_assoc()) {
+            //var_dump($row);
+            $sla[] = $row;
+        }
+        echo json_encode($sla);
+        header('Content-Type: application/json');
+        //header('location: teste.php');
+        
+    }
+    
+    tratar();
+?>
+    
